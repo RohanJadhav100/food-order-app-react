a595f1bd4434217f7f932e7c83eccb44aeb8e1f6
Rohan Jadhav <135609778+RohanJadhav100@users.noreply.github.com>
2023-08-01 16:33:32 +0530

    Feature - Manage Cart OPen/Close State (#1)
    * open cart state
    
    * modal state handle
    
    * handled close cart state
    
    ---------
    
    Co-authored-by: Rohan Jadhav <rohanjadhav7069@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
-import { Fragment } from "react";
+import { Fragment, useState } from "react";
 import Header from "./components/Layout/Header";
 import Meal from "./components/Meal/Meals";
+import Cart from "./components/Cart/Cart";
 
 function App() {
+  const [cartState, setCartState] = useState(false);
+
+  const openCartHandler = () => {
+    setCartState(true);
+  };
+
+  const closeCartHandler = () => {
+    setCartState(false);
+  };
   return (
     <Fragment>
-      <Header />
+      {cartState && <Cart onCloseCart={closeCartHandler} />}
+      <Header onCartOpen={openCartHandler} />
       <main>
         <Meal />
       </main>
